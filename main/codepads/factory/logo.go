4494e1acf5d19cb2ecbab368fca833e8020b9981
AGulev <me@agulev.com>
2026-01-20 20:40:34 +0100

    update to be compatible with defold 1.12.1

--- main/codepads/factory/logo.go
+++ main/codepads/factory/logo.go
@@ -1,17 +1,6 @@
 components {
   id: "go"
   component: "/codepad/go.script"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
   properties {
     id: "script_id"
     value: "logo"
@@ -21,20 +10,11 @@ components {
 embedded_components {
   id: "sprite"
   type: "sprite"
-  data: "tile_set: \"/main/assets/logo.atlas\"\n"
-  "default_animation: \"logo\"\n"
+  data: "default_animation: \"logo\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
-  "blend_mode: BLEND_MODE_ALPHA\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/assets/logo.atlas\"\n"
+  "}\n"
   ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
